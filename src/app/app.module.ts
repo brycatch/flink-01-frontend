d0fcf995b9066541a7b72ad8609b6cd170093b76
Bryan Alejandro Sanchez Ramirez <sarabryanale@outlook.com>
2021-01-17 19:55:05 -0600

    IMPROVE: Dashboard and create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { CreateComponent } from './components/create/create.component';
 import { ListComponent } from './components/list/list.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
+
+
 import { DatePipe } from './pipes/date/date.pipe';
 
 @NgModule({
@@ -20,7 +23,7 @@ import { DatePipe } from './pipes/date/date.pipe';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
